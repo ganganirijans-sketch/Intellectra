4ba7192d465ae8cdbf81d315c3a0e4c45c7cc7e8
ganganirijans-sketch <ganganirijans@gmail.com>
2026-05-17 21:47:35 +0530

    fix: redirect immediately when deleting active chat

--- components/sidebar/chat-menu-item.tsx
+++ components/sidebar/chat-menu-item.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { useCallback, useState, useTransition } from 'react'
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
+import { useCallback, useState, useTransition } from 'react'
 
 import { MoreHorizontal, Trash2 } from 'lucide-react'
 import { toast } from 'sonner'
